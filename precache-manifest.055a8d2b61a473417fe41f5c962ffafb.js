63c05400ea14ef0f87fb00200c44b7aa2f936240
Santiago Ferreira <san650@gmail.com>
2019-06-30 15:21:14 -0300

    Updates

---
+++ precache-manifest.055a8d2b61a473417fe41f5c962ffafb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42001cee7410daa687886bb94386fb0",
+    "url": "/workout/index.html"
+  },
+  {
+    "revision": "382ecc2f9611eba5894e",
+    "url": "/workout/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/workout/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "382ecc2f9611eba5894e",
+    "url": "/workout/static/js/main.e466405a.chunk.js"
+  },
+  {
+    "revision": "645ef791f3102785dfb8",
+    "url": "/workout/static/js/runtime~main.787068e9.js"
+  }
+]);
