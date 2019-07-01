74323b1967f46bd774fa386c02849676a7b7f5df
Santiago Ferreira <san650@gmail.com>
2019-06-30 22:29:25 -0300

    Collapse weeks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import data from './data';
 import StopWatch from './components/stopwatch';
@@ -14,15 +14,36 @@ function App() {
   );
 }
 
-function Training({ title, weeks }) {
-  return (
-    <div>
-      <h2>
-        {title}
-      </h2>
-      {weeks.map((week) => <Week key={week.title} {...week} />)}
-    </div>
-  );
+class Training extends Component {
+  state = {
+    weeks: this.props.weeks.map((week) => ({
+      ...week,
+      isVisible: false
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>
+          {this.props.title}
+        </h2>
+        {this.state.weeks.map((week) =>
+          <Week key={week.title} {...week} onVisibilityChange={this.handleVisibilityChange} />
+        )}
+      </div>
+    );
+  }
+
+  handleVisibilityChange = (target) => {
+    this.setState({
+      ...this.state,
+      weeks: this.state.weeks.map((week) =>
+        week.title !== target ?
+          week : { ...week, isVisible: !week.isVisible }
+      )
+    });
+  }
 }
 
 const weekStyle = {
@@ -36,19 +57,31 @@ const helpStyle = {
   fontWeight: 'normal',
 };
 
-function Week({ title, days, exercises }) {
-  return (
-    <div style={weekStyle}>
-      <h3>
-        {title}
-        <br />
-        <span style={helpStyle}>
-          {days}
-        </span>
-      </h3>
-      {exercises.map((exercise) => <Exercise key={exercise.name} {...exercise} />)}
-    </div>
-  );
+class Week extends Component {
+  render() {
+    const { title, days, exercises, isVisible } = this.props;
+
+    return (
+      <div style={weekStyle}>
+        <h3 onClick={this.handleClick}>
+          {title}
+          {isVisible && <br />}
+          {isVisible &&
+            <span style={helpStyle}>
+              {days}
+            </span>
+          }
+        </h3>
+        {isVisible && exercises.map((exercise) =>
+          <Exercise key={exercise.name} {...exercise} />)
+        }
+      </div>
+    );
+  }
+
+  handleClick = () => {
+    this.props.onVisibilityChange(this.props.title);
+  }
 }
 
 function LabelAndValue({ label, value }) {
